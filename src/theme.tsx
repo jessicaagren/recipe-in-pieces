ff13a8b1c7eba70435cebbff339d51a4c2a4c9b0
jessicaagren <jessicaagren@hotmail.com>
2026-03-19 22:30:44 +0100

    add theme colours

---
+++ src/theme.tsx
@@ -0,0 +1,73 @@
+// src/theme.ts
+import { createTheme } from '@mantine/core';
+export const theme = createTheme({
+  cursorType: 'pointer',
+  colors: {
+    yellow: [
+      '#fff8db',
+      '#fff1b8',
+      '#ffe98f',
+      '#ffe066',
+      '#ffd83d',
+      '#FFBE0B',
+      '#e6aa0a',
+      '#cc9709',
+      '#b38308',
+      '#997007',
+    ],
+
+    orange: [
+      '#ffe6d9',
+      '#ffccb3',
+      '#ffb38c',
+      '#ff9966',
+      '#ff8040',
+      '#FB5607',
+      '#e04d06',
+      '#c54405',
+      '#aa3b04',
+      '#8f3203',
+    ],
+
+    pink: [
+      '#ffd9e7',
+      '#ffb3cf',
+      '#ff8cb8',
+      '#ff66a0',
+      '#ff4089',
+      '#FF006E',
+      '#e60063',
+      '#cc0058',
+      '#b3004d',
+      '#990042',
+    ],
+
+    violet: [
+      '#ede3ff',
+      '#dbc7ff',
+      '#c9abff',
+      '#b78fff',
+      '#a573ff',
+      '#8338EC',
+      '#7632d4',
+      '#692dbc',
+      '#5c27a4',
+      '#4f228c',
+    ],
+
+    blue: [
+      '#e0edff',
+      '#c2dbff',
+      '#a3c9ff',
+      '#85b7ff',
+      '#66a5ff',
+      '#3A86FF',
+      '#3478e6',
+      '#2e6acc',
+      '#285cb3',
+      '#224e99',
+    ],
+  },
+
+  primaryColor: 'pink',
+});
