c79d007ffa2aac63e873299a138cf340480aabdf
jessicaagren <jessicaagren@hotmail.com>
2026-03-19 22:31:28 +0100

    add MantineProvider with theme import

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { theme } from './theme.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 );
