45ca7a959d7edf3e7d3fb2d141f1fdd2afd65fd9
jessicaagren <jessicaagren@hotmail.com>
2026-03-20 19:41:28 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import './App.scss';
+import AppShellComponent from './Components/AppShell/AppShell';
 
-function App() {
-  return <div>Recipe in Pieces</div>;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './Components/Routes/HomePage';
+import RecipePage from './Components/Routes/RecipePage';
+import { categories } from './Data/categories';
+import { recipes } from './Data/recipes';
+import CategoryPage from './Components/Routes/CategoryPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppShellComponent />,
+    children: [
+      { index: true, element: <HomePage /> },
+
+      ...recipes.map(({ path, id }) => ({
+        path,
+        element: <RecipePage recipeId={id} />,
+      })),
+
+      ...categories.map(({ path }) => ({
+        path,
+        element: <CategoryPage />,
+      })),
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
