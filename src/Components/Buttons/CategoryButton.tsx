583090c9f930e13c9802f09d0868b0ca5d1c9e66
jessicaagren <jessicaagren@hotmail.com>
2026-03-21 11:25:43 +0100

    remove unused import

--- src/Components/Buttons/CategoryButton.tsx
+++ src/Components/Buttons/CategoryButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@mantine/core';
 import type { Category } from '../../Data/categories';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 type CategoryButtonProps = {
   path?: string;
