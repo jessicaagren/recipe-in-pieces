d39040996b68f841d0dbdd650d54446284951c75
jessicaagren <jessicaagren@hotmail.com>
2026-03-21 14:46:01 +0100

    add 'new recipe' route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import RecipePage from './Components/Routes/RecipePage';
 import { categories } from './Data/categories';
 import { recipes } from './Data/recipes';
 import CategoryPage from './Components/Routes/CategoryPage';
+import FormPage from './Components/Routes/FormPage';
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,8 @@ const router = createBrowserRouter([
         path,
         element: <CategoryPage />,
       })),
+
+      { path: '/nytt-recept', element: <FormPage /> },
     ],
   },
 ]);
