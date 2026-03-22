27c84e8fdf8cbcfc4aa7dcb515d2c3787fb6f02a
jessicaagren <jessicaagren@hotmail.com>
2026-03-22 16:53:58 +0100

    edit scroll styling and transform on components

--- src/Components/AppShell/AppShell.tsx
+++ src/Components/AppShell/AppShell.tsx
@@ -10,54 +10,60 @@ import {
 import { categories } from '../../Data/categories';
 import CategoryMenu from '../CategoryMenu/CategoryMenu';
 import { Link, Outlet } from 'react-router-dom';
-import { useHeadroom } from '@mantine/hooks';
 import PlusButton from '../Buttons/PlusButton';
+import { useWindowScroll } from '@mantine/hooks';
 
 export default function AppShellComponent() {
-  const pinned = useHeadroom({ fixedAt: 0 });
+  const [scroll] = useWindowScroll();
+
+  const isSmall = scroll.y > 120;
 
   return (
-    <AppShell
-      p='lg'
-      header={{
-        height: pinned ? 180 : 80,
-      }}
-      footer={{ height: 60 }}>
+    <AppShell p='lg' footer={{ height: 60 }}>
       <AppShell.Header
-        p='inherit'
-        m='0'
-        bg='pink'
-        bd='none'
         style={{
-          transition: 'all 0.3s ease',
+          height: isSmall ? 80 : 180,
+          transition: 'height 0.3s ease',
           overflow: 'hidden',
-        }}>
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1000,
+          border: 'none',
+        }}
+        p='lg'
+        bg='pink'>
         <Stack>
-          <Title order={1}>
-            <Anchor component={Link} to={'/'} variant='text' c='dark' size='xl'>
-              {/* Recipe in Pieces */}
-              <Image
-                src='/src/assets/logo1.svg'
-                mah={60}
-                w={200}
-                alt='Recipe in Pieces logo'
-                pb={pinned ? 0 : 'lg'}
-                style={{
-                  objectFit: 'contain',
-                  transition: 'all 0.3s ease',
-                }}
-              />
-            </Anchor>
-          </Title>
+          <Group>
+            <Title order={1}>
+              <Anchor
+                component={Link}
+                to={'/'}
+                variant='text'
+                c='dark'
+                size='xl'>
+                {/* Recipe in Pieces */}
+                <Image
+                  src='/src/assets/logo1.svg'
+                  alt='Recipe in Pieces logo'
+                  style={{
+                    height: isSmall ? 40 : 60,
+                    transition: 'all 0.3s ease',
+                    objectFit: 'contain',
+                  }}
+                />
+              </Anchor>
+            </Title>
+          </Group>
           <Group
-            visibleFrom='sm'
             mt='xs'
             justify='space-between'
             style={{
-              opacity: pinned ? 1 : 0,
-              transform: pinned ? 'translateY(0)' : 'translateY(-100px)',
-              transition: 'all 0.3s ease',
-              pointerEvents: pinned ? 'auto' : 'none',
+              opacity: isSmall ? 0 : 1,
+              transform: isSmall ? 'translateY(-20px)' : 'translateY(0)',
+              transition: 'all 0.2s ease',
+              pointerEvents: isSmall ? 'none' : 'auto',
             }}>
             <CategoryMenu categories={categories} />
             <PlusButton />
@@ -67,12 +73,12 @@ export default function AppShellComponent() {
 
       {/* <AppShell.Navbar>Navbar</AppShell.Navbar> */}
 
-      <AppShell.Main pt='var(--app-shell-header-height)'>
+      <AppShell.Main pt={isSmall ? 80 : 180}>
         <div style={{ paddingBottom: 80, paddingTop: 20 }}>
           <Outlet />
         </div>
       </AppShell.Main>
-      <AppShell.Footer p='md' bg='pink' bd='none'>
+      <AppShell.Footer p='md' bg='pink' bd='none' mb='-1px'>
         <Text c='white' ta='center' size='sm'>
           Recepten och bilder är inlagda av ~moi~.
         </Text>
